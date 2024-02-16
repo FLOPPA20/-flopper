a5653bc57e94468e4f6b820f29fc0dc3616eac84
FLOPPA20 <136246475+FLOPPA20@users.noreply.github.com>
2024-02-16 22:32:30 +0300

    Add files via upload

---
+++ botflopper/bot.py
@@ -0,0 +1,37 @@
+import time
+import random
+from config import token
+#!/usr/bin/python
+
+# This is a simple echo bot using the decorator mechanism.
+# It echoes any incoming text messages.
+
+import telebot
+
+
+
+bot = telebot.TeleBot(token)
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+@bot.message_handler(commands=["rand"])
+def rand(message):
+    bot.send_chat_action(message.chat.id, 'typing')
+    time.sleep(5)
+    bot.send_message(message.chat.id,random.randint(0,1000))
+@bot.message_handler(comands=[""])
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+
+
+bot.infinity_polling()
